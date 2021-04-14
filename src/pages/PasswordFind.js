945066d4040aa4f44990ed2ae9b8a338aa8141ac
ouo_yoonk <joyk9251@gmail.com>
2021-04-14 17:22:03 +0900

    apply find password and update password api styling PasswordFind page

--- src/pages/PasswordFind.js
+++ src/pages/PasswordFind.js
@@ -14,53 +14,49 @@ const PasswordSearch = ({ history }) => {
   // status : input(이메일입력) -> auth(인증) -> update(변경)
   const [status, setStatus] = useState('input');
   const [isError, setIsError] = useState(null);
+
+  const [email, setEmail, onChangeEmail] = useInput('');
+  const [number, setNumber, onChangeNumber] = useInput('');
+  const [password, setPassword, onChangePassword] = useInput('');
   const [passwordChk, setPasswordChk, onChangePasswordChk] = useInput('');
-  const [email, setEmail] = useState('');
-  const [value, setValue, onChangeValue] = useInput('');
 
   const onClickButton = () => {
     if (status === 'input') {
-      handleInputStatus();
-    } else if (status === 'auth') {
-      handleAuthStatus();
+      checkAuthNumber();
     } else if (status === 'update') {
-      handleUpdateStatus();
+      updatePassword();
     }
   };
 
-  const handleInputStatus = () => {
+  const sendAuthMail = () => {
     dispatch(userActions.setAuthNumber(null));
-    //setStatus('auth');
-    if (!testEmailValid(value)) {
+
+    if (!testEmailValid(email)) {
       return setIsError('이메일 형식으로 입력해주세요');
     }
 
-    setEmail(value);
-    setValue('');
     setIsError(null);
-    dispatch(userActions.findPassword(value));
-    setStatus('auth');
+    dispatch(userActions.findPassword(email));
   };
 
-  const handleAuthStatus = () => {
-    if (value === authNumber) {
+  const checkAuthNumber = () => {
+    setIsError(null);
+    if (number === authNumber) {
       setStatus('update');
     } else {
       setIsError('인증번호가 일치하지 않습니다');
     }
   };
 
-  const handleUpdateStatus = () => {
-    const data = { email, password: value };
-    dispatch(userActions.updatePassword(data));
-  };
-
-  const onAuth = () => {
-    setStatus('update');
-  };
-
   const updatePassword = () => {
-    console.log('변경하기');
+    setIsError(null);
+
+    if (password !== passwordChk) {
+      return setIsError('동일한 비밀번호를 입력해주세요');
+    }
+
+    const data = { email, password };
+    dispatch(userActions.updatePassword(data));
   };
 
   const goBack = () => {
@@ -75,40 +71,57 @@ const PasswordSearch = ({ history }) => {
   return (
     <Container>
       <Title>패스워드 찾기</Title>
-      <Wrapper height="100px">
-        <Wrapper is_column>
-          <Input
-            value={value}
-            _onChange={onChangeValue}
-            placeholder={
-              status === 'input'
-                ? '이메일을 입력해주세요'
-                : status === 'auth'
-                ? '이메일로 전송된 인증번호를 입력해주세요'
-                : '새 비밀번호를 입력해주세요'
-            }
-          ></Input>
-          {status === 'update' && (
+      <OutterWrapper>
+        <InnerWrapper status={status}>
+          <Stage>
+            <Wrapper margin="0.5em 0">
+              <Input
+                value={email}
+                _onChange={onChangeEmail}
+                placeholder="이메일을 입력해주세요"
+              ></Input>
+              <Wrapper width="40%">
+                <Button _onClick={sendAuthMail}>인증메일 전송</Button>
+              </Wrapper>
+            </Wrapper>
+            {authNumber && (
+              <Input
+                _onChange={onChangeNumber}
+                placeholder="인증번호를 입력해주세요"
+              ></Input>
+            )}
+          </Stage>
+
+          <Stage>
             <Wrapper margin="0.5rem 0">
               <Input
-                placeholder="동일한 비밀번호를 입력해주세요"
-                _onChange={onChangePasswordChk}
+                type="password"
+                _onChange={onChangePassword}
+                placeholder="변경할 비밀번호를 입력해주세요"
               ></Input>
             </Wrapper>
-          )}
-        </Wrapper>
-      </Wrapper>
+            <Input
+              type="password"
+              _onChange={onChangePasswordChk}
+              placeholder="동일한 비밀번호를 입력해주세요"
+            ></Input>
+          </Stage>
+        </InnerWrapper>
+      </OutterWrapper>
       <Wrapper margin="0.5rem 0">
         <ErrorMsg valid={isError}>{isError}</ErrorMsg>
       </Wrapper>
-      <Wrapper margin="1rem 0">
-        <Button _onClick={goBack}>이전단계</Button>
-        <Button _onClick={onClickButton}>
-          {status === 'input'
-            ? '인증 메일 전송'
-            : status === 'auth'
-            ? '인증하기'
-            : '비밀번호 변경'}
+      <Wrapper margin="1rem">
+        <Button _onClick={goBack}>
+          {status === 'input' ? '취소' : '이전단계'}
+        </Button>
+        <Button
+          disabled={
+            status === 'input' ? !authNumber : !password || !passwordChk
+          }
+          _onClick={onClickButton}
+        >
+          {status === 'input' ? '인증하기' : '비밀번호 변경'}
         </Button>
       </Wrapper>
     </Container>
@@ -117,6 +130,7 @@ const PasswordSearch = ({ history }) => {
 
 const Container = styled.div`
   width: 350px;
+  height: 100%;
 
   ${(props) => props.theme.flex_column};
   justify-content: center;
@@ -130,7 +144,7 @@ const Title = styled.span`
 `;
 
 const OutterWrapper = styled.div`
-  width: 350px;
+  width: 400px;
   overflow: hidden;
 `;
 
@@ -140,15 +154,11 @@ const InnerWrapper = styled.div`
   width: calc(350 * 3px);
   transition: 0.3s;
   transform: ${(props) =>
-    props.status === 'input'
-      ? `translateX(0px)`
-      : props.status === 'auth'
-      ? `translateX(-350px)`
-      : `translateX(-700px)`};
+    props.status === 'input' ? `translateX(0px)` : `translateX(-400px)`};
 `;
 
 const Stage = styled.div`
-  width: 300px;
+  width: 350px;
   margin: 0 25px;
 `;
 
