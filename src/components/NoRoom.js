dac6a2ea7e11ec4d2f53d5710657e9fab2a15eff
ouo_yoonk <joyk9251@gmail.com>
2021-04-16 15:50:46 +0900

    add noRoom main

--- src/components/NoRoom.js
+++ src/components/NoRoom.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const NoRoom = (props) => {
   return (
     <Container>
-      <img src="../asset/miniproject-7.png"></img>
+      <img src="miniproject-7.png"></img>
     </Container>
   );
 };
@@ -13,6 +13,9 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${(props) => props.theme.main_color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default NoRoom;
